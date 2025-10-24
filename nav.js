ae0e1b9aaf02297bfaf9ec06954065015317d09a
Thomas Jones <tommyjonesjr11@gmail.com>
2025-10-24 14:39:56 -0400

    Lab 6 Part 1

---
+++ nav.js
@@ -0,0 +1,22 @@
+function splitAtRoot(path) {
+  const url = new URL(path, location.origin);
+  return url.pathname;
+}
+function setNav(current_path) {
+  fetch("nav.html")
+    .then(r => r.text())
+    .then(html => {
+      const navElem = document.getElementById("main-nav");
+      navElem.innerHTML = html;
+      current_path = splitAtRoot(current_path);
+      for (const link of navElem.querySelectorAll("a")) {
+        const href_clean = splitAtRoot(link.href);
+        const isHome =
+          (current_path === "/" && href_clean.endsWith("/index.html")) ||
+          (href_clean === current_path);
+
+        if (isHome) link.classList.add("current"); // use your site’s .current class
+      }
+    })
+    .catch(err => console.log("nav load error:", err));
+}
