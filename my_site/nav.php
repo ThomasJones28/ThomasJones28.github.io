353e78b358b7e41c94224514df72bf375a7811de
Thomas Jones <tommyjonesjr11@gmail.com>
2025-11-08 16:22:06 -0500

    Lab8 part 1 bonus

--- my_site/nav.php
+++ my_site/nav.php
@@ -1,7 +1,12 @@
 <nav>
-  <a href="index.php">Home</a>
-  <a href="my_artistic_self.php">My Artistic Self</a>
-  <a href="my_vacation.php">My Vacation</a>
+  <a href="index.php" class="logo"><img src="images/logo.png" alt="TJ logo" style="height:40px;"></a>
+  <div class="dropdown">
+    <button class="dropbtn">Discover Me!</button>
+    <div class="dropdown-content">
+        <a href="my_artistic_self.php">My Artistic Self</a>
+        <a href="my_vacation.php">My Vacation</a>
+    </div>
+  </div>
   <a href="marketplace.php">Marketplace</a>
   <a href="calculators.php">Calculators</a>
   <a href="my_form.php">My Form</a>
